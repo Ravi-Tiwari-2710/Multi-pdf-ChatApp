c6c9d698353bf8476faab579d7794e032c2143bb
RAVI TIWARI <84631546+Ravi-Tiwari-2710@users.noreply.github.com>
2023-12-03 20:10:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     #embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+    embeddings = HuggingFaceEmbeddings(model_name="HuggingFaceH4/zephyr-7b-beta",
                                    model_kwargs={'device':"cpu"})
     #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
