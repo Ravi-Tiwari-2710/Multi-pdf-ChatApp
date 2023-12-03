c55f83cebc549e4f23330cafec49b9fb60de00af
RAVI TIWARI <84631546+Ravi-Tiwari-2710@users.noreply.github.com>
2023-12-03 21:12:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
    # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    llm = HuggingFaceHub(repo_id="microsoft/Orca-2-13b", model_kwargs={"temperature":0.5, "max_length":512})
 
 
 
